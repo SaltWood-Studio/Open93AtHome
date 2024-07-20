da46b6b948cfe8f3cb2b5b0305ea6c7f2a2ad96d
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-07-20 22:37:10 +0800

    fix: 没提交到

--- src/SocketIOServer.java
+++ src/SocketIOServer.java
@@ -48,6 +48,7 @@ public class SocketIOServer {
                     this.sessions.put(client.getSessionId().toString(), id);
                 }
             }
+            client.sendEvent("message", "Welcome to Open93@Home! You can find us at https://github.com/SaltWood-Studio/Open93AtHome.");
             System.out.println("Client connected: " + client.getSessionId());
         });
         
