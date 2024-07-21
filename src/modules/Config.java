2975453e94b7ad3a8d9fa1e7fa66b639a61aa47c
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-07-21 18:49:40 +0800

    Release 1.1.0

--- src/modules/Config.java
+++ src/modules/Config.java
@@ -2,6 +2,6 @@ package modules;
 
 public class Config {
     public String filePath = "./files";
-    public String version = "1.0.1";
+    public String version = "1.1.0";
     public String userAgent = "93@home-ctrl/" + version;
 }
